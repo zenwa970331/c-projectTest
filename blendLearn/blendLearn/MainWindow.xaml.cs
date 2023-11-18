142bfeb48b9e5095834c54f98e1a2c14932dae87
wzh <1050027459@qq.com>
2023-11-19 00:40:40 +0800

    按钮移动1

--- blendLearn/blendLearn/MainWindow.xaml.cs
+++ blendLearn/blendLearn/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,7 @@ namespace blendLearn
         public MainWindow()
         {
             InitializeComponent();
+            //x_move.Completed
         }
 
         private void fan_MouseEnter(object sender, MouseEventArgs e)
@@ -33,6 +35,21 @@ namespace blendLearn
         private void fan_MouseLeave(object sender, MouseEventArgs e)
         {
             VisualStateManager.GoToState(fan, "stop", false);
+        }
+
+
+
+        DoubleAnimation x_move = new DoubleAnimation()
+        {
+            Duration = new TimeSpan(0, 0, 0, 0, 50),
+        };
+        private void Run(UIElement element, double to)
+        {
+            lock (element)
+            {
+                x_move.To = to;
+                element.BeginAnimation(Canvas.LeftProperty, x_move);
+            }
         }
     }
 }
